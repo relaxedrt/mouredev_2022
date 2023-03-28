71b1b4b2c5ef879668b4e8fd0ea6852b08877ee3
relaxedrt <rodrigotejeria01@gmail.com>
2023-03-28 19:57:02 +0200

    explicado y completados dos ejercicios

--- 10_EXPRESIONESEQUILIBRADAS/equiexpr.py
+++ 10_EXPRESIONESEQUILIBRADAS/equiexpr.py
@@ -6,16 +6,22 @@
 
 exp = "{ [ a * ( c + d ) ] - 5 }"
 badexp = "{ a * ( c + d ) ] - 5 }"
+
+#creamos una lista con los simbolos que queremos respetar
 lcor = [ "{", "[", "(", ")", "]", "}"]
 
 def equilibrator(xp):
+    #borramos los espacios de la expesión
     xpr = xp.replace(" ", "")
     cor = []
+    #añadimos cada caracter a una lista si se encuentra en nuestra lista de simbolos
     for i in xpr:
         if i in lcor:
             cor.append(i)
     print(cor)
+    #recorremos los simbolos, si hay un simbolo, tiene que haber uno igual en su posición simetrica
     for i in range(0, len(cor)+1):
+        #mientras recorramos la mitad de los simbolos comprobamos
         while i < ((len(cor))/2):
             print(cor[i], cor[-1-i])
             if (cor[i] == "{") and (cor[-1-i] != "}"):
@@ -25,6 +31,7 @@ def equilibrator(xp):
             if (cor[i] == "(") and (cor[-1-i] != ")"):
                 return False
             break
+        #si la mitad de los simbolos han sido correctos, la otra mitad tambien ya que son simetricos
         if i >((len(cor) + 1)/2):
             return True
 
